44b7c0c242dc41802f913e2f268d654d0e98d5fc
Ibtida Bin Ahmed <108508643+Ibtida01@users.noreply.github.com>
2023-09-30 10:51:31 +0600

    Update 20_21_6a.cpp

--- 20_21_6a.cpp
+++ 20_21_6a.cpp
@@ -1,52 +1,64 @@
+import java.util.ArrayList;
 
 class StringTokenizer {
     String s;
     char delim;
     int tokenCount;
-    int currpos;
-
+    //int currpos;
+    String[] tokens;
+    int currentToken;
     StringTokenizer(String s, char delim) {
         this.s = s;
         this.delim = delim;
         tokenCount = 0;
-        currpos = 0;
+        //currpos = 0;
+        currentToken=-1;
+        tokenize(s);
+    }
+    private void tokenize(String s)
+    {
+       String[] temp=s.split(String.valueOf(delim),-1);
+        for(String str:temp)
+        {
+            if(str.length()>0)
+            {
+                tokenCount++;
+            }
+        }
+        tokens=new String[tokenCount];
+        int i=0;
+        for(String str:temp)
+        {
+            if(str.length()>0)
+            {
+                tokens[i++]=str;
+            }
+        }
     }
     int countTokens()
     {
-        int count = 0;
-        for(int i = 0; i < s.length(); i++)
-        {
-            if(s.charAt(i) == delim)
-                count++;
-        }
-        return count + 1;
+       return tokenCount;
     }
     boolean hasMoreTokens()
     {
-        if(currpos < s.length())
+        if(currentToken < tokenCount-1)
             return true;
         //else
             return false;
     }
     String nextToken()
     {
-        int i=-1;
-        for( i=currpos;i<s.length();i++)
+        if(currentToken<tokenCount-1)
         {
-            if(s.charAt(i)==delim)
-            {
-                break;
-            }
+            return tokens[++currentToken];
         }
-        String currentToken=s.substring(currpos,i);
-        currpos=i+1;
-        return currentToken;
+        else return null;
     }
 }
 
 public class tf {
     public static void main(String[] args) {
-        String s = "This is a test";
+        String s = "This is   a    test";
         StringTokenizer st = new StringTokenizer(s, ' ');
         System.out.println(st.countTokens());
         while(st.hasMoreTokens())
